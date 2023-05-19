f8678c01d4842ebf197e96f0c9400ec06b7684e9
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2023-05-19 16:06:50 +0200

    je sais plus ou j'en suis

---
+++ map.js
@@ -0,0 +1,9 @@
+<script>
+  function initMap() {
+    // Créez une nouvelle instance de carte
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644}, // Coordonnées du centre de la carte
+      zoom: 8 // Niveau de zoom initial
+    });
+  }
+</script>
